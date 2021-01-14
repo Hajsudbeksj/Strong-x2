2ff9b144c707309d14fcf433af5aeb05f1470c55
Hajsudbeksj <77440522+Hajsudbeksj@users.noreply.github.com>
2021-01-14 13:54:55 +0200

    Add files via upload

---
+++ strong.py
@@ -0,0 +1,43 @@
+import os
+print(">_ STRONG X2")
+print("""───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+""")
+yes=("y")
+no=("n")
+name=input("Do you want to continue? [y/n]")
+if name == no :
+	os.system("exit")
+elif name == yes :
+	print ("ok")
+	os.system ("pkg install git")
+	os.system ("pkg install python3")
+	os.system ("pkg install update")
+	os.system ("pkg install upgrade")
+	os.system ("pkg install python2")
+	os.system ("pkg install ruby")
+	os.system ("pkg install php")
+	os.system ("pkg install java")
+	os.system ("pkg install bash")
+	os.system ("pkg install perl")
+	os.system ("pkg install nmap")
+	os.system ("pkg install bash")
+	os.system ("pkg install clang")
+	os.system ("pkg install macchanger")
+	os.system ("pkg install nano")
+	os.system ("pkg install figlet")
+	os.system ("pkg install cowsay")
+	os.system ("pkg install curl")
+	os.system ("pkg install tar")
+	os.system ("pkg install zip")
+	os.system ("pkg install unzip")
+	os.system ("pkg install tor")
+	os.system ("pkg install sudo")
+	os.system ("pkg install wget")
+	os.system ("pkg install wcalc")
+	os.system ("pkg install openssl")
+	os.system ("pkg install bmon")
+print("good bay :)")
